c3602f9b755a2648434caa210bf76ce50bcb145a
Rosa.Master <edu.wd2014@gmail.com>
2022-04-12 00:54:27 -0300

    atualização shell aula 30

--- core/models.py
+++ core/models.py
@@ -6,8 +6,14 @@ class Produto(models.Model):
     preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em Estoque')
 
+    def __str__(self):
+        return f'{self.nome}'
+
 
 class Cliente(models.Model):
     nome = models.CharField('Nome', max_length=100)
     sobrenome = models.CharField('Sobrenome', max_length=100)
     email = models.EmailField('E-mail', max_length=100)
+
+    def __str__(self):
+        return f'{self.nome} {self.sobrenome} {self.email}'
--- core/views.py
+++ core/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 
 
 def index(request):
+    print(dir(request))
+    print(f"User: {request.user.last_name}")
+
+    if str(request.user) == 'AnonymousUser':
+        teste = 'Usuário não logado'
+    else:
+        teste = 'Usuário Logado'
     context = {
         'curso': 'Programação Web com Django Framework',
-        'outro': 'Django é muito bom !!!'
+        'outro': 'Django é muito bom !!!',
+        'logado': teste
     }
     return render(request, 'index.html', context)
 
